bfb111ebd117713947b1dee5e072123eabe1094d
vimpower <phaminhieu1996@gmail.com>
2024-08-25 23:42:10 +0800

    fix cache issue

--- main.py
+++ main.py
@@ -60,7 +60,9 @@ dtype_spec = {
 if "last_fetch_time" not in st.session_state:
     st.session_state.last_fetch_time = 0
 
-@st.cache_data(ttl=60)
+
+
+@st.cache_data(max_entries=2, ttl=60, show_spinner=True)
 def fetch_trains():
     for _ in range(100):
         if time.time() - st.session_state.last_fetch_time > 60: 
@@ -84,9 +86,6 @@ def fetch_trains():
                 return df
             else:
                 time.sleep(10.0)
-        else:
-            print("Reuseing data ...")
-            return st.session_state.train_df
     
 
 
@@ -100,8 +99,6 @@ routes_trip_df = pd.merge(trips_df, routes_df, how='inner')
 
 # Streamlit app layout
 
-if 'train_df' not in st.session_state:
-    st.session_state.train_df = fetch_trains()
 
 if 'active_tab' not in st.session_state:
     st.session_state.active_tab = 'Tab 1'
@@ -113,9 +110,9 @@ with tab1:
     if st.session_state.active_tab != 'Tab 1':
         st.session_state.active_tab = 'Tab 1'
     st.title("Train Tracker")
-    selected_line = st.selectbox("Select a route to track", options=routes_df['route_long_name'])
+    selected_line = st.selectbox("Select a route to track", key='line', options=routes_df['route_long_name'])
     direction_options = sorted(selected_line.replace("KTM ", "").replace("Intercity ", "").replace("Electric Train Service", "").split(" - "))
-    selected_direction = st.selectbox('Train travel from', options=direction_options)
+    selected_direction = st.selectbox('Train travel from', key='direction', options=direction_options)
 
     routes_trip_df[['start_station', 'end_station']] = routes_trip_df['route_long_name'].str.replace("KTM ", "").str.split(' - ', expand=True)
     swap_condition = routes_trip_df['direction_id'] == 1
@@ -135,77 +132,90 @@ with tab1:
 with tab2:
     if st.session_state.active_tab != 'Tab 2':
         st.session_state.active_tab = 'Tab 2'
-    selected_train = st.selectbox('Train label', options=st.session_state.train_df.sort_values(by='label', ascending=False)['label'])
-    placeholder = st.empty()
+
+    train_df = fetch_trains()
+    selected_train = st.selectbox('Train label', key='train', options=train_df.sort_values(by='label', ascending=False)['label'])
+
+    placeholder1 = st.empty()
+    placeholder2 = st.empty()
+    placeholder3 = st.empty()
+    placeholder4 = st.empty()
+
+    placeholder5 = st.empty()
+    placeholder6 = []
+    for i in range(len(station_df)):
+        placeholder6.append(st.empty())
+
+
     while st.session_state.active_tab == 'Tab 2': 
-        with placeholder.container():
-            train_df = fetch_trains()
+        train_df = fetch_trains()
 
-            train_data_df = train_df[train_df['label'] == selected_train]
-            train_data_sequence = train_data_df.squeeze()
-            st.write(train_data_df)
+        train_data_df = train_df[train_df['label'] == selected_train]
+        train_data_sequence = train_data_df.squeeze()
+        placeholder1.write(train_data_df)
 
 
-            # Find best trains to match the schedule
-            df = pd.merge(st.session_state.train_df, routes_trip_df, left_on='tripId',right_on='trip_id', how='inner')
+        # Find best trains to match the schedule
+        df = pd.merge(train_df, routes_trip_df, left_on='tripId',right_on='trip_id', how='inner')
+        if selected_direction is not None:
             df_train_candidates = df.loc[df['start_station'] == selected_direction]
-            st.write('Best candidates')
-            st.write(df_train_candidates[['label', 'latitude', 'longitude', 'speed', 'route_long_name']])
+            placeholder2.write('Best candidates')
+            placeholder3.write(df_train_candidates[['label', 'latitude', 'longitude', 'speed', 'route_long_name']])
 
-            train_data = train_data_sequence.to_dict()
+        train_data = train_data_sequence.to_dict()
 
-            geo_df = station_df[['stop_lat', 'stop_lon']].copy()
-            stations_lat_lon = geo_df.to_dict(orient='records')
+        geo_df = station_df[['stop_lat', 'stop_lon']].copy()
+        stations_lat_lon = geo_df.to_dict(orient='records')
 
-            nearest_station_index = find_nearest_point(train_data, stations_lat_lon)
+        nearest_station_index = find_nearest_point(train_data, stations_lat_lon)
 
-            if pd.isna(first_match_trip['route_color']):
-                first_match_trip['route_color'] = 'FF0000'
-            geo_df['color'] = f"#{first_match_trip['route_color']}"
+        if pd.isna(first_match_trip['route_color']):
+            first_match_trip['route_color'] = 'FF0000'
+        geo_df['color'] = f"#{first_match_trip['route_color']}"
+        if first_match_trip['direction_id'] == 1:
+            geo_df.iloc[:nearest_station_index, geo_df.columns.get_loc('color')] = '#00FF00'
+        else:
+            geo_df.iloc[nearest_station_index + 1:, geo_df.columns.get_loc('color')] = '#00FF00'
+
+        geo_df['size'] = '100'
+        geo_df = pd.concat(
+            [pd.DataFrame([[train_data['latitude'], train_data['longitude'], '#FFFFFF',200]], columns=geo_df.columns), geo_df], ignore_index=True)
+        placeholder4.map(geo_df, latitude='stop_lat', longitude='stop_lon', color='color')
+
+
+        # # Display the stations on a straight line
+        placeholder5.markdown(
+            f"""
+            <div style="text-align:right; display: flex; justfy-content: left">
+                <div style="width: 10rem;margin-right: 10px;white-space: normal;">Distance [km]</div>
+                <div style="width: 10rem;margin-right: 10px;white-space: normal;">Time estimated [min]</div>
+                <div style="width: 1rem;margin-right: 10px;white-space: normal;">Status</div>
+                <div style="width: 10rem;margin-right: 10px;white-space: normal;">Station</div>
+            </div>
+            """, 
+            unsafe_allow_html=True)
+        for station_id, station in enumerate(station_df['stop_name'].squeeze().to_list()):
+            color = f"#{first_match_trip['route_color']}"
             if first_match_trip['direction_id'] == 1:
-                geo_df.iloc[:nearest_station_index, geo_df.columns.get_loc('color')] = '#00FF00'
+                if station_id < nearest_station_index:
+                    color = "#00FF00"
+                if station_id == nearest_station_index:
+                    color = "#FFFFFF"
+
             else:
-                geo_df.iloc[nearest_station_index + 1:, geo_df.columns.get_loc('color')] = '#00FF00'
+                if station_id > nearest_station_index:
+                    color = "#00FF00"
+                if station_id == nearest_station_index:
+                    color = "#FFFFFF"
 
-            geo_df['size'] = '100'
-            geo_df = pd.concat(
-                [pd.DataFrame([[train_data['latitude'], train_data['longitude'], '#FFFFFF',200]], columns=geo_df.columns), geo_df], ignore_index=True)
-            st.map(geo_df, latitude='stop_lat', longitude='stop_lon', color='color')
-
-
-            # # Display the stations on a straight line
-            st.markdown(
+            placeholder6[station_id].markdown(
                 f"""
                 <div style="text-align:right; display: flex; justfy-content: left">
-                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">Distance [km]</div>
-                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">Time estimated [min]</div>
-                    <div style="width: 1rem;margin-right: 10px;white-space: normal;">Status</div>
-                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">Station</div>
+                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">{station_df.loc[station_id, 'relative_time'].total_seconds() // 60}</div>
+                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">{round(station_df.loc[station_id, 'relative_distance'], 2)}</div>
+                    <div style="width:1rem; height:1rem; margin:auto; background-color:{color}; border-radius:50%;"></div>
+                    <div style="width: 10rem;margin-right: 10px;white-space: normal;">{string.capwords(station)}</div>
                 </div>
                 """, 
                 unsafe_allow_html=True)
-            for station_id, station in enumerate(station_df['stop_name'].squeeze().to_list()):
-                color = f"#{first_match_trip['route_color']}"
-                if first_match_trip['direction_id'] == 1:
-                    if station_id < nearest_station_index:
-                        color = "#00FF00"
-                    if station_id == nearest_station_index:
-                        color = "#FFFFFF"
-
-                else:
-                    if station_id > nearest_station_index:
-                        color = "#00FF00"
-                    if station_id == nearest_station_index:
-                        color = "#FFFFFF"
-
-                st.markdown(
-                    f"""
-                    <div style="text-align:right; display: flex; justfy-content: left">
-                        <div style="width: 10rem;margin-right: 10px;white-space: normal;">{station_df.loc[station_id, 'relative_time'].total_seconds() // 60}</div>
-                        <div style="width: 10rem;margin-right: 10px;white-space: normal;">{round(station_df.loc[station_id, 'relative_distance'], 2)}</div>
-                        <div style="width:1rem; height:1rem; margin:auto; background-color:{color}; border-radius:50%;"></div>
-                        <div style="width: 10rem;margin-right: 10px;white-space: normal;">{string.capwords(station)}</div>
-                    </div>
-                    """, 
-                    unsafe_allow_html=True)
-            time.sleep(5)
+        time.sleep(5)
